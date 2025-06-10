eb4bb9913a905edf625a47251a7fa39deb0b9b73
iamselimj <iamselimj@proton.me>
2025-06-10 04:25:14 +0200

    feat:minor changes

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 13:56:41 by stempels          #+#    #+#             */
-/*   Updated: 2025/06/10 04:14:04 by user             ###   ########.fr       */
+/*   Updated: 2025/06/10 04:24:34 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,6 @@ int	minishell(int mode, char **env)
 	shell = init_shell(mode, env);
 	while (1)
 	{
-		shell->tokens = NULL;
-		shell->tree = NULL;
 		display_prompt();
 		shell->cli = readline("\033[1;32m$\033[0m ");
 		if (!shell->cli)
@@ -32,24 +30,28 @@ int	minishell(int mode, char **env)
 			printf("%sLEAVING the minishell...\n", BOLD_RED);
 			break ;
 		}
+		if (!is_valid_cli(shell->cli))
+		{
+			printf("%sUnclosed quotes\n", BOLD_RED);
+		}
 		add_history(shell->cli);
 		shell->tokens = lexer(&shell->tokens, shell->cli);
 		if (!shell->tokens)
 			return (1);
-		if (mode == 1 || (mode >= 2 && mode != 4))
+		if (shell->mode == 1 || (shell->mode >= 2 && shell->mode != 4))
 			show_lexeme(shell->tokens);
 		shell->tree = parser(shell->tokens);
 		if (!shell->tree)
 			return (1);
-		if (mode == 1 || mode >= 3)
+		if (shell->mode == 1 || shell->mode >= 3)
 			show_tree(shell->tree, 1);
-		if (mode <= 1)
+		if (shell->mode <= 1)
 		{
 			pid = fork();
 			if (pid < 0)
 				return (1);
 			else if (pid == 0)
-				execute(shell->tree, env);
+				execute(shell->tree, envp_from_env(shell->env));
 			else
 				wait(&pid);
 		}
