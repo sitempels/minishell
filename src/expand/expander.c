57aff0a2aa376cecbc7ed5b9957b6d17f006ce3e
Simon Tempels <stempels@shi-r12-p1.s19.be>
2025-06-25 18:37:17 +0200

    expander: reworking it from the ground

--- src/expand/expander.c
+++ src/expand/expander.c
@@ -6,7 +6,7 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 16:18:04 by stempels          #+#    #+#             */
-/*   Updated: 2025/06/25 17:31:46 by stempels         ###   ########.fr       */
+/*   Updated: 2025/06/25 18:34:55 by stempels         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -126,6 +126,54 @@ char	*process_arg(t_token *arg, t_env *env, int status)
 	expanded = expand_string(raw, env, status);
 	free(raw);
 	return (expanded);
+}
+
+char	*expand_arg(t_shell *shell, t_token *arg, t_env *env, int status, int *quoted)
+{
+	size_t	len;
+	size_t	i;
+	char	*var;
+
+	len = 0;
+	i = 0;
+	while (i < arg->size)
+	{
+		if (arg->start[i] == '\'' || arg->size[i] == '\"')
+		{
+			*quoted = 1;
+			i++;
+		}
+		if (arg->start[i] == '$')
+		{
+			i++;
+			len += get_new_size(shell, arg, i, *quoted)
+		}
+
+	}
+var = env_getone(env, arg->start[i], )
+}
+
+size_t	get_new_size(t_shell *shell, t_token *arg, size_t i, int *quoted)
+{
+	t_env	*tmp;
+	size_t	len;
+	
+	if (i == arg->size)
+		return (0);
+	len = i;
+	if (arg->start[i] == '?')
+		len = ft_intlen_base(shell->status, BASE_10);
+	else if (ft_isalpha(arg->start[i]) || arg->start[i] == '_')
+	{
+		while (!match(arg->size[i], DELIMITERS))
+			i++;
+		tmp = env_getone(shell->env, arg->start[len], i - len);
+		if (!tmp)
+			return (0);
+		len = ft_strlen(tmp->value)	
+	}
+	len += get_new_size(shell, arg, i, *quoted);
+	return (len);
 }
 
 char	**get_arg(t_token *arg, int nbr, t_env *env, int status)
