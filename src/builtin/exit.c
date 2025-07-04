00d5aeff449a422d764ea5c1268ba6f2527fd57b
iamselimj <iamselimj@proton.me>
2025-07-04 22:37:28 +0200

    extra: change exit status msg

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/09 21:42:56 by user              #+#    #+#             */
-/*   Updated: 2025/06/25 13:58:33 by stempels         ###   ########.fr       */
+/*   Updated: 2025/07/04 22:36:54 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 int	builtin_exit(t_shell *shell, int print, int status)
 {
 	if (print)
-		ft_printf("exit\n");
+		ft_printf("minishell: exiting the shell...");
 	shell->status = status;
 	(void)shell;
 	destroy_shell(shell);
