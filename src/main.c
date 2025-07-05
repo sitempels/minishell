82053343a29297cb1265558031ccb5d6762a05e2
Simon Tempels <stempels@shi-r12-p1.s19.be>
2025-07-05 11:30:18 +0200

    corrected redir

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 11:31:51 by stempels          #+#    #+#             */
-/*   Updated: 2025/07/04 22:12:56 by user             ###   ########.fr       */
+/*   Updated: 2025/07/05 11:28:37 by stempels         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,15 +55,19 @@ static int	parse_and_execute(t_shell *shell)
 
 static void	wait_and_restore(t_shell *shell)
 {
+	int	fd;
+
 	while (shell->child_nbr > 0)
 	{
 		wait_and_decrypt_child(shell);
 		shell->child_nbr--;
 	}
-	close(STDOUT_FILENO);
-	open(shell->std_io[0], O_RDWR);
-	close(STDIN_FILENO);
-	open(shell->std_io[1], O_RDWR);
+	fd = open(shell->std_io[0], O_RDWR);
+	dup2(fd, STDOUT_FILENO);
+	close(fd);
+	fd = open(shell->std_io[1], O_RDWR);
+	dup2(fd, STDIN_FILENO);
+	close(fd);
 	clean_shell(shell);
 }
 
