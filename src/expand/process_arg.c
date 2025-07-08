af5ae7a166a88cbe85221688a39139f29b4456a7
Simon Tempels <stempels@shi-r12-p1.s19.be>
2025-07-08 14:43:27 +0200

    quote handling corrected

--- src/expand/process_arg.c
+++ src/expand/process_arg.c
@@ -6,7 +6,7 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 06:25:00 by user              #+#    #+#             */
-/*   Updated: 2025/07/05 10:27:51 by stempels         ###   ########.fr       */
+/*   Updated: 2025/07/08 14:41:49 by stempels         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,17 +47,32 @@ static void	cpy_and_add(char *raw, char *start, size_t size)
 {
 	size_t	i;
 	size_t	j;
+	char	quote;
 
 	i = 0;
 	j = 0;
-	while (i < size)
+	while (i + j < size)
 	{
-		if (start[i] == '\'' || start[i] == '\"')
+		if (start[i] && (start[i] == '\'' || start[i] == '\"'))
 		{
+			quote = start[i];
 			raw[i + j] = '&';
 			j++;
+			raw[i + j] = start[i];
+			i++;
+			while (start[i] && start[i] != quote)
+			{
+				raw[i + j] = start[i];
+				i++;
+			}
+			raw[i + j] = '&';
+			j++;
+			raw[i + j] = start[i];
+			i++;
+			j++;
 		}
-		raw[i + j] = start[i];
+		else
+			raw[i + j] = start[i];
 		i++;
 	}
 }
